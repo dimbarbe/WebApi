3c245138857cc965bb20eed26dad4a53294d3639
julitogtu <julito_gtu@hotmail.com>
2013-11-09 11:20:58 -0500

    Custom PropertyName

---
+++ CustomObjectSerialization/Controllers/ClientController.cs
@@ -0,0 +1,18 @@
+﻿using CustomObjectSerialization.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace CustomObjectSerialization.Controllers
+{
+    public class ClientController : ApiController
+    {
+        private readonly List<Client> clients = new List<Client>(){
+            new Client(){ ClientId = 1, Name = "Julio", LastName = "Avellaneda", City  = "Bogotá", Password = "123456"}
+        };
+
+        public IEnumerable<Client> GetClients()
+        {
+            return clients;
+        }
+    }
+}
--- CustomObjectSerialization/Models/Client.cs
+++ CustomObjectSerialization/Models/Client.cs
@@ -1,15 +1,23 @@
-﻿
+﻿using Newtonsoft.Json;
+
 namespace CustomObjectSerialization.Models
 {
     public class Client
     {
+        [JsonProperty(PropertyName="Id")]
         public int ClientId { get; set; }
-        
+
+        [JsonProperty(PropertyName = "Nombre")]
         public string Name { get; set; }
 
+        [JsonProperty(PropertyName = "Apellido")]
         public string LastName { get; set; }
-        
+
+        [JsonProperty(PropertyName = "Ciudad")]
         public string City { get; set; }
+        
+        [JsonIgnore]
+        public string Password { get; set; }
 
     }
 }
