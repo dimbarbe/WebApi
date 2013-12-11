0543cec9998e42f3a8882c2bd0da97dec763d6c7
julitogtu <julito_gtu@hotmail.com>
2013-12-11 06:58:57 -0500

    Add example interface IHttpActionResult

---
+++ InterfaceIHttpResult/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿
+namespace InterfaceIHttpResult.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Twitter { get; set; }
+    }
+}
---
+++ InterfaceIHttpResult/Models/PersonDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace InterfaceIHttpResult.Models
+{
+    public class PersonDBContext : DbContext
+    {
+        public DbSet<Person> Person { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
